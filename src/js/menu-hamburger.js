cd88a5c43dd4f4f6f17620e04370e542de6e393e
Bruno Realan Sopeña <brunorealans@gmail.com>
2025-09-10 16:31:42 -0300

    Add (fonts, shadows,some buttons)

--- src/js/menu-hamburger.js
+++ src/js/menu-hamburger.js
@@ -1,10 +1,18 @@
-    const btn = document.querySelector('button[aria-controls="mobile-menu"]');
-    const menu = document.getElementById('mobile-menu');
-    const open = document.getElementById('icon-open');
-    const close = document.getElementById('icon-close');
+const menu = document.getElementById('mobile-menu');
+const openBtn = document.getElementById('icon-open');
+const closeBtn = document.getElementById('icon-close');
 
-    btn.addEventListener('click', () => {
-      menu.classList.toggle('hidden');
-      open.classList.toggle('hidden');
-      close.classList.toggle('hidden');
-    });
+openBtn.addEventListener('click', () => {
+  menu.classList.remove('hidden', 'scale-y-0', 'opacity-0');
+  menu.classList.add('scale-y-100', 'opacity-100');
+  openBtn.classList.add('hidden');
+  closeBtn.classList.remove('hidden');
+});
+
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('scale-y-100', 'opacity-100');
+  menu.classList.add('scale-y-0', 'opacity-0');
+  setTimeout(() => menu.classList.add('hidden'), 300); // espera la transición
+  closeBtn.classList.add('hidden');
+  openBtn.classList.remove('hidden');
+});
